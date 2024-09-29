24a1bc8d936e7cf96161b2cc4733cb8520daceca
Ikbal <ikbalnayem000@gmail.com>
2024-09-29 17:03:32 +0600

    handle cors

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import { connectToDatabase } from './config/db-connect';
@@ -10,6 +11,7 @@ const PORT = parseInt(process.env.PORT || '4500');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
