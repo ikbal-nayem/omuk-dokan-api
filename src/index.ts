bc50692e361808c3bfd2437637e67fd2c5152944
Ikbal <ikbalnayem000@gmail.com>
2024-12-09 22:49:00 +0600

    remove CORS handler

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const PORT = parseInt(process.env.PORT || '4500');
 const app = express();
 
 app.use(cors());
-app.use(CORShandler);
+// app.use(CORShandler);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
