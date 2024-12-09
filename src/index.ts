e1253c42b570d25bf22d70d1cc1efe9121b2b3c6
Ikbal <ikbalnayem000@gmail.com>
2024-12-09 22:12:28 +0600

    a

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,7 @@ const PORT = parseInt(process.env.PORT || '4500');
 
 const app = express();
 
-app.use(
-  cors(),
-);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
